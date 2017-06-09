fbc29903e28a4d37a97486cfe9e7b5bafd563133
cecolby11 <cecolby11@users.noreply.github.com>
2017-06-09 12:25:50 -0500

    heroku procfile and port setup

--- server.js
+++ server.js
@@ -217,7 +217,8 @@ app.delete('/articles/:articleId/comment/:commentId', function(req, res) {
 });
 
 // Listen on port 8080
-app.listen(8080, function() {
+var port = (process.env.PORT || 5000);
+app.listen(port, function() {
   console.log("App running on port!");
 });
 
